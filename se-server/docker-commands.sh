eff5b291ef03e90436c5a7e7af770e57d5597a53
praakhya <67508200+praakhya@users.noreply.github.com>
2023-11-17 00:55:36 +0530

    Hint for tag format.

--- se-server/docker-commands.sh
+++ se-server/docker-commands.sh
@@ -1,4 +1,7 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker build .
+
+docker tag SOURCE-IMAGE LOCATION-docker.pkg.dev/PROJECT-ID/REPOSITORY/IMAGE:TAG
+
 docker tag se-server-server:latest us-west1-docker.pkg.dev/involuted-disk-405316/se-project/se-server 
 docker push us-west1-docker.pkg.dev/involuted-disk-405316/se-project/se-server
